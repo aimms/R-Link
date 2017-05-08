7f19a4224b55f3a56f155c651b87b739b89eaf12
mihalis dorgiakis <mihalis.dorgiakis>
2017-05-08 17:15:18 +0200

    Removed RFunctional Model from the repo, added the sample RAimmsTest project

--- RFunctionalModel/RScripts/initialize3DIdentifier.r
+++
@@ -1,4 +0,0 @@
-integers=c(1,2,3);
-cities=c('Amsterdam','Haarlem');
-doubles=c(3.1, 7.2, 8.3);
-values = vector(length = length(integers) * length(cities) * length(doubles) );
--- RFunctionalModel/RScripts/rearrange3DDataFrame.r
+++
@@ -1,3 +0,0 @@
-df = expand.grid(doubles,cities,integers)
-df[4] = values
-df = df[,c(3,2,1,4)]
